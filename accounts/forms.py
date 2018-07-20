0d259a4e2023ee8695e9257c9345e8fecd260c47
Rohan Krishna <rohan@Rohans-MacBook-Air.local>
2018-07-20 21:53:51 +0530

    A small change in accounts model form added.

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class UserLoginForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = User
         fields = ('username','password')
